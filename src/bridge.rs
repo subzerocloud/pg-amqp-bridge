d0c2cc4032652364b54df457a704adfe8f266c0f
steve-chavez <stevechavezast@gmail.com>
2017-05-25 12:55:43 -0500

    Refactor parse functions and add unit tests

--- src/bridge.rs
+++ src/bridge.rs
@@ -11,7 +11,7 @@ use std::string::String;
 use tokio_core::reactor::Core;
 use tokio_core::net::TcpStream;
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 struct Bridge{
   pg_channel: String,
   amqp_entity: String,
@@ -20,19 +20,16 @@ struct Bridge{
 #[derive(Debug)]
 struct Envelope{
   pg_channel: String,
-  routing_key: RoutingKey,
-  message: Message,
+  routing_key: String,
+  message: String,
   amqp_entity: String,
 }
 
-type RoutingKey = String;
-type Message = String;
-
-const SEPARATOR: &str = "|";
+const SEPARATOR: char = '|';
 
 pub fn start_bridge(amqp_host_port: &String, pg_uri: &String, bridge_channels: &String){
   let amqp_addr: SocketAddr = amqp_host_port.parse().expect("amqp_host_port should be in format '127.0.0.1:5672'");
-  let bridges = parse_bridges(bridge_channels);
+  let bridges = parse_bridge_channels(bridge_channels);
 
   let (sender, receiver): (Sender<Envelope>, Receiver<Envelope>) = channel();
 
@@ -43,15 +40,19 @@ pub fn start_bridge(amqp_host_port: &String, pg_uri: &String, bridge_channels: &
   amqp_publisher(amqp_addr, receiver);
 }
 
-fn parse_bridges(bridge_channels: &str) -> Vec<Bridge>{
+fn parse_bridge_channels(bridge_channels: &str) -> Vec<Bridge>{
   let mut bridges: Vec<Bridge> = Vec::new();
   let str_bridges: Vec<Vec<&str>> = bridge_channels.split(",").map(|s| s.split(":").collect()).collect();
   for i in 0..str_bridges.len(){
-    bridges.push(Bridge{pg_channel: str_bridges[i][0].to_string(),
-                        amqp_entity: if str_bridges[i].len() > 1 {str_bridges[i][1].to_string()} else {String::new()}
+    bridges.push(Bridge{pg_channel: str_bridges[i][0].trim().to_string(),
+                        amqp_entity: str_bridges[i].get(1).unwrap_or(&"").trim().to_string()
                  });
   }
-  bridges
+  let flt_bs : Vec<Bridge> = bridges.into_iter().filter(|x| !x.pg_channel.is_empty()).collect();
+  if flt_bs.len() == 0 { 
+    panic!("No postgresql channel specified in \"{}\"", bridge_channels) 
+  }
+  flt_bs
 }
 
 fn spawn_pg_listener(pg_uri: String, bridge: Bridge, sender: Sender<Envelope>){
@@ -64,10 +65,10 @@ fn spawn_pg_listener(pg_uri: String, bridge: Bridge, sender: Sender<Envelope>){
     loop {
       match it.next() {
         Ok(Some(notification)) => {
-          let (routing_key, message) = parse_payload(&notification.payload);
+          let (routing_key, message) = parse_notification(&notification.payload);
           let envelope = Envelope{pg_channel: bridge.pg_channel.clone(),
-                                  routing_key: routing_key,
-                                  message: message,
+                                  routing_key: routing_key.to_string(),
+                                  message: message.to_string(),
                                   amqp_entity: bridge.amqp_entity.clone()};
           debug!("Sent: {:?}", envelope);
           if let Err(e) = sender.send(envelope) {
@@ -82,12 +83,12 @@ fn spawn_pg_listener(pg_uri: String, bridge: Bridge, sender: Sender<Envelope>){
   ()
 }
 
-fn parse_payload(payload: &String) -> (RoutingKey, Message){
-  let v: Vec<&str> = payload.split(SEPARATOR).collect();
+fn parse_notification(payload: &str) -> (&str, &str){
+  let v: Vec<&str> = payload.splitn(2, SEPARATOR).map(|x| x.trim()).collect();
   if v.len() > 1 {
-    (v[0].to_string(), v[1].to_string())
+    (v[0], v[1])
   } else {
-    (String::new(), v[0].to_string())
+    ("", v[0])
   }
 }
 
@@ -117,4 +118,50 @@ fn amqp_publisher(amqp_addr: SocketAddr, receiver: Receiver<Envelope>){
       Ok(channel)
     })
   );
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn parse_notification_works() {
+    assert!(("my_key", "A message") == parse_notification("my_key|A message"));
+    assert!(("my_key", "A message") == parse_notification("  my_key  |  A message  "));
+    assert!(("my_key", "A message|Rest of message") == parse_notification("my_key|A message|Rest of message"));
+    assert!(("", "my_key##A message") == parse_notification("my_key##A message"));
+    assert!(("", "A message") == parse_notification("A message"));
+    assert!(("", "") == parse_notification(""));
+    assert!(("mý_kéý", "A mésságé") == parse_notification("mý_kéý|A mésságé"));
+  }
+
+  #[test]
+  fn parse_bridge_channels_works() {
+    assert!(vec![Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string()}] 
+            == parse_bridge_channels("pgchannel1:exchange1"));
+    assert!(vec![
+              Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string()},
+              Bridge{pg_channel: "pgchannel2".to_string(), amqp_entity: "exchange2".to_string()}
+            ] == parse_bridge_channels("pgchannel1:exchange1,pgchannel2:exchange2"));
+    assert!(vec![
+              Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string()},
+              Bridge{pg_channel: "pgchannel2".to_string(), amqp_entity: "exchange2".to_string()},
+              Bridge{pg_channel: "pgchannel3".to_string(), amqp_entity: "exchange3".to_string()}
+            ] == parse_bridge_channels(" pgchannel1 : exchange1 , pgchannel2 : exchange2 , pgchannel3 : exchange3 "));
+    assert!(vec![
+              Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "".to_string()},
+              Bridge{pg_channel: "pgchannel2".to_string(), amqp_entity: "".to_string()},
+              Bridge{pg_channel: "pgchannel3".to_string(), amqp_entity: "exchange3".to_string()}
+            ] == parse_bridge_channels("pgchannel1,pgchannel2:,pgchannel3:exchange3,"));
+  }
+
+  use std::panic::catch_unwind;
+
+  #[test]
+  fn parse_bridge_channels_panics_if_no_pg_channel() {
+    assert!(catch_unwind(|| parse_bridge_channels("   ")).is_err());
+    assert!(catch_unwind(|| parse_bridge_channels(":exchange1")).is_err());
+    assert!(catch_unwind(|| parse_bridge_channels(":exchange1,:exchange2")).is_err());
+    assert!(catch_unwind(|| parse_bridge_channels(",:,:exchange3,,")).is_err());
+  }
 }
