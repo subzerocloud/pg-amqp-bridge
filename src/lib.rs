54a063d4eaf71c0be253f4cb35f75d546f5ba69c
steve-chavez <stevechavezast@gmail.com>
2017-06-09 12:55:34 -0500

    Change interface to "pgchannel1:queue1,pgchannel2:exchange1"

--- src/lib.rs
+++ src/lib.rs
@@ -9,43 +9,42 @@ use std::default::Default;
 use std::thread;
 use std::thread::JoinHandle;
 
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+enum Type {
+  Exchange,
+  Queue
+}
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 struct Bridge{
   pg_channel: String,
   amqp_entity: String,
-}
-
-#[derive(Debug)]
-struct Envelope<'a>{
-  pg_channel: &'a str,
-  routing_key: &'a str,
-  message: &'a str,
-  amqp_entity: &'a str,
+  amqp_entity_type: Option<Type>
 }
 
 const SEPARATOR: char = '|';
 
 pub fn start_bridge(amqp_host_port: &String, pg_uri: &String, bridge_channels: &String){
-  let bridges = parse_bridge_channels(bridge_channels);
-
+  let mut bridges = parse_bridge_channels(bridge_channels);
   let mut children = Vec::new();
 
   let mut session = Session::open_url(amqp_host_port.as_str()).unwrap();
-  let mut ids = 0;
+  let mut channel_id = 0;
 
-  for bridge in &bridges{
-    ids += 1;
-    let channel1 = session.open_channel(ids).unwrap();
-    ids += 1;
-    let channel2 = session.open_channel(ids).unwrap();
-    if !amqp_entity_exists(channel1, channel2, &bridge.amqp_entity){
+  for bridge in &mut bridges{
+    channel_id += 1; let channel1 = session.open_channel(channel_id).unwrap();
+    channel_id += 1; let channel2 = session.open_channel(channel_id).unwrap();
+    let amqp_entity_type = amqp_entity_type(channel1, channel2, &bridge.amqp_entity);
+    if amqp_entity_type.is_none(){
       panic!("The amqp entity {:?} doesn't exist", bridge.amqp_entity);
+    }else{
+      bridge.amqp_entity_type = amqp_entity_type;
     }
   }
 
   for bridge in bridges{
-    ids += 1;
-    let channel = session.open_channel(ids).unwrap();
+    channel_id += 1; let channel = session.open_channel(channel_id).unwrap();
     children.push(spawn_listener_publisher(channel, pg_uri.clone(), bridge));
   }
 
@@ -54,44 +53,49 @@ pub fn start_bridge(amqp_host_port: &String, pg_uri: &String, bridge_channels: &
   }
 }
 
-
-/*
- * Validates using two channels because currently rust-amqp hangs up when doing exchange_declare
- * and queue_declare on the samme channel.
- * It validates with passive set to true.
-*/
-fn amqp_entity_exists(mut channel1: Channel, mut channel2: Channel, amqp_entity: &String) -> bool{
-  let it_exists = channel1.queue_declare(amqp_entity.clone(), true, false, false, false, false, Table::new()).is_ok() ||
-                  channel2.exchange_declare(amqp_entity.clone(), "".to_string(), true, false, false, false, false, Table::new()).is_ok();
-  channel1.close(200, "").unwrap();
-  channel2.close(200, "").unwrap();
-  it_exists
-}
-
 fn spawn_listener_publisher(mut channel: Channel, pg_uri: String, bridge: Bridge) -> JoinHandle<()>{
   thread::spawn(move ||{
     let pg_conn = Connection::connect(pg_uri, TlsMode::None).expect("Could not connect to PostgreSQL");
 
     let listen_command = format!("LISTEN {}", bridge.pg_channel);
-    pg_conn.execute(listen_command.as_str(), &[]).expect("Could not send LISTEN");
+    pg_conn.execute(listen_command.as_str(), &[]).unwrap();
 
     println!("Listening on {}...", bridge.pg_channel);
 
     let notifications = pg_conn.notifications();
     let mut it = notifications.blocking_iter();
+
+    let amqp_entity_type = bridge.amqp_entity_type.unwrap();
+
     while let Ok(Some(notification)) = it.next() {
       let (routing_key, message) = parse_notification(&notification.payload);
-      let envelope = Envelope{pg_channel: &bridge.pg_channel,
-                              routing_key: &routing_key,
-                              message: &message,
-                              amqp_entity: &bridge.amqp_entity};
-      channel.basic_publish(envelope.amqp_entity, envelope.routing_key, true, false,
+      let (exchange, key) =  if amqp_entity_type == Type::Exchange {
+                               (bridge.amqp_entity.as_str(), routing_key)
+                             } else {
+                               ("", bridge.amqp_entity.as_str())
+                             };
+      channel.basic_publish(exchange, key, true, false,
                             protocol::basic::BasicProperties{ content_type: Some("text".to_string()), ..Default::default()},
-                            envelope.message.as_bytes().to_vec()).ok().expect("Failed publishing");
-      println!("Forwarding {:?} from pg channel {:?} to exchange {:?} with routing key {:?} ",
-               envelope.message, envelope.pg_channel, envelope.amqp_entity, envelope.routing_key);
+                            message.as_bytes().to_vec()).unwrap();
+      println!("Forwarding {:?} from pg channel {:?} to {:?} {:?} with routing key {:?} ",
+               message, bridge.pg_channel, amqp_entity_type, bridge.amqp_entity, routing_key);
     }
   })
+}
+
+/*
+ * Finds the amqp entity type(Queue or Exchange) using two channels because currently rust-amqp hangs up when doing exchange_declare
+ * and queue_declare on the samme channel.
+ * It does this with amqp "passive" set to true.
+*/
+fn amqp_entity_type(mut channel1: Channel, mut channel2: Channel, amqp_entity: &String) -> Option<Type>{
+  let opt_queue_type = channel1.queue_declare(amqp_entity.clone(), true, false, false, false, false, Table::new())
+                       .map(|_| Type::Queue).ok();
+  let opt_exchange_type = channel2.exchange_declare(amqp_entity.clone(), "".to_string(), true, false, false, false, false, Table::new())
+                          .map(|_| Type::Exchange).ok();
+  channel1.close(200, "").unwrap();
+  channel2.close(200, "").unwrap();
+  opt_queue_type.or(opt_exchange_type)
 }
 
 fn parse_bridge_channels(bridge_channels: &str) -> Vec<Bridge>{
@@ -99,22 +103,24 @@ fn parse_bridge_channels(bridge_channels: &str) -> Vec<Bridge>{
   let str_bridges: Vec<Vec<&str>> = bridge_channels.split(",").map(|s| s.split(":").collect()).collect();
   for i in 0..str_bridges.len(){
     bridges.push(Bridge{pg_channel: str_bridges[i][0].trim().to_string(),
-                        amqp_entity: str_bridges[i].get(1).unwrap_or(&"").trim().to_string()
+                        amqp_entity: str_bridges[i].get(1).unwrap_or(&"").trim().to_string(),
+                        amqp_entity_type: None
                  });
   }
-  let mut flt_bs : Vec<Bridge> = bridges.into_iter().filter(|x| !x.pg_channel.is_empty()).collect();
-  if flt_bs.len() == 0 {
-    panic!("No postgresql channel specified in \"{}\"", bridge_channels)
+  let mut cleaned_bridges : Vec<Bridge> = bridges.into_iter().filter(|x| !x.pg_channel.is_empty() && !x.amqp_entity.is_empty())
+                                          .collect();
+  if cleaned_bridges.len() == 0 {
+    panic!("No bindings(e.g. pgchannel1:queue1) specified in \"{}\"", bridge_channels)
   }
-  flt_bs.sort();
-  flt_bs.dedup_by(|a, b|{
+  cleaned_bridges.sort();
+  cleaned_bridges.dedup_by(|a, b|{
     let is_dup = a.pg_channel == b.pg_channel;
     if is_dup{
       panic!("Cannot have duplicate PostgreSQL channels.");
     }
     is_dup
   });
-  flt_bs
+  cleaned_bridges
 }
 
 fn parse_notification(payload: &str) -> (&str, &str){
@@ -143,32 +149,29 @@ mod tests {
 
   #[test]
   fn parse_bridge_channels_works() {
-    assert!(vec![Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string()}]
+    assert!(vec![Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string(), amqp_entity_type: None}]
             == parse_bridge_channels("pgchannel1:exchange1"));
     assert!(vec![
-              Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string()},
-              Bridge{pg_channel: "pgchannel2".to_string(), amqp_entity: "exchange2".to_string()}
+              Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string(), amqp_entity_type: None},
+              Bridge{pg_channel: "pgchannel2".to_string(), amqp_entity: "exchange2".to_string(), amqp_entity_type: None}
             ] == parse_bridge_channels("pgchannel1:exchange1,pgchannel2:exchange2"));
     assert!(vec![
-              Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string()},
-              Bridge{pg_channel: "pgchannel2".to_string(), amqp_entity: "exchange2".to_string()},
-              Bridge{pg_channel: "pgchannel3".to_string(), amqp_entity: "exchange3".to_string()}
-            ] == parse_bridge_channels(" pgchannel1 : exchange1 , pgchannel2 : exchange2 , pgchannel3 : exchange3 "));
-    assert!(vec![
-              Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "".to_string()},
-              Bridge{pg_channel: "pgchannel2".to_string(), amqp_entity: "".to_string()},
-              Bridge{pg_channel: "pgchannel3".to_string(), amqp_entity: "exchange3".to_string()}
-            ] == parse_bridge_channels("pgchannel1,pgchannel2:,pgchannel3:exchange3,"));
+              Bridge{pg_channel: "pgchannel1".to_string(), amqp_entity: "exchange1".to_string(), amqp_entity_type: None},
+              Bridge{pg_channel: "pgchannel2".to_string(), amqp_entity: "exchange2".to_string(), amqp_entity_type: None},
+              Bridge{pg_channel: "pgchannel3".to_string(), amqp_entity: "exchange3".to_string(), amqp_entity_type: None}
+            ] == parse_bridge_channels(" pgchannel1 : exchange1 , pgchannel2 : exchange2 , pgchannel3 : exchange3, "));
   }
 
   use std::panic::catch_unwind;
 
   #[test]
-  fn parse_bridge_channels_panics_if_no_pg_channel() {
+  fn parse_bridge_channels_panics_if_no_pg_channel_and_exchange_specified() {
     assert!(catch_unwind(|| parse_bridge_channels("   ")).is_err());
+    assert!(catch_unwind(|| parse_bridge_channels(":")).is_err());
+    assert!(catch_unwind(|| parse_bridge_channels("pgchannel1")).is_err());
     assert!(catch_unwind(|| parse_bridge_channels(":exchange1")).is_err());
-    assert!(catch_unwind(|| parse_bridge_channels(":exchange1,:exchange2")).is_err());
-    assert!(catch_unwind(|| parse_bridge_channels(",:,:exchange3,,")).is_err());
+    assert!(catch_unwind(|| parse_bridge_channels("pgchannel1:")).is_err());
+    assert!(catch_unwind(|| parse_bridge_channels("pgchannel1, pgchannel1:, :exchange3,,")).is_err());
   }
 
   #[test]
--- tests/main.rs
+++ tests/main.rs
@@ -19,9 +19,16 @@ use std::thread;
 use test::*;
 use bridge::start_bridge;
 
+//Lapin doesn't support amqp://localhost// format.
 const TEST_AMQP_HOST_PORT: &str = "127.0.0.1:5672";
 const TEST_PG_URI: &str = "postgres://postgres@localhost";
-const TEST_BRIDGE_CHANNELS: &str = "pgchannel1,pgchannel2:pg_amqp_bridge_direct_exchange,pgchannel3:pg_amqp_bridge_topic_exchange";
+const TEST_BRIDGE_CHANNELS: &str = "pgchannel1:pg_amqp_bridge_queue_1,pgchannel2:pg_amqp_bridge_direct_exchange";
+
+/*
+ * We use lapin for all the tests since rust-amqp has no way to exit a consumer
+ * without panicking, this doesn't let the tests do assert! reliably.
+ * See issue: https://github.com/Antti/rust-amqp/issues/23
+*/
 
 fn publishing_to_queue_works() {
   const TEST_QUEUE: &str = "pg_amqp_bridge_queue_1";
@@ -37,22 +44,21 @@ fn publishing_to_queue_works() {
     TcpStream::connect(&addr, &handle)
     .and_then(|stream| Client::connect(stream, &ConnectionOptions::default()) )
     .and_then(|client| client.create_channel())
-    .and_then(|channel|{
-      let ch1 = channel.clone();
-      let ch2 = channel.clone();
+    .and_then(|channel|
       channel.queue_declare(TEST_QUEUE, &QueueDeclareOptions::default(), FieldTable::new())
-      .and_then(move |_| channel.basic_consume(TEST_QUEUE, "my_consumer_1", &BasicConsumeOptions::default()))
-      .and_then(move |stream| {
-        let _ = pg_conn.execute(format!("NOTIFY {}, '{}|Queue test'", TEST_PG_CHANNEL, TEST_QUEUE).as_str(), &[]);
-        stream.into_future().map_err(|(err, _)| err)
-      })
-      .and_then(move |(message, _)| {
-        let msg = message.unwrap();
-        assert_eq!(msg.data, b"Queue test");
-        ch1.basic_ack(msg.delivery_tag)
-      })
-      .then(move |_| ch2.queue_delete(TEST_QUEUE, &QueueDeleteOptions::default()))
-    })
+      .and_then(move |_| 
+        channel.basic_consume(TEST_QUEUE, "my_consumer_1", &BasicConsumeOptions::default())
+        .and_then(move |stream|{
+          pg_conn.execute(format!("NOTIFY {}, '{}|Queue test'", TEST_PG_CHANNEL, TEST_QUEUE).as_str(), &[]).unwrap();
+          stream.into_future().map_err(|(err, _)| err)
+          .and_then(move |(message, _)| {
+            let msg = message.unwrap();
+            assert_eq!(msg.data, b"Queue test");
+            channel.basic_ack(msg.delivery_tag)
+          })
+        })
+      )
+    )
   );
 }
 
@@ -99,11 +105,114 @@ fn publishing_to_direct_exchange_works() {
   );
 }
 
-fn publishing_to_topic_exchange_works() {
-  const TEST_QUEUE_1: &str = "pg_amqp_bridge_queue_3";
-  const TEST_QUEUE_2: &str = "pg_amqp_bridge_queue_4";
-  const TEST_EXCHANGE: &str = "pg_amqp_bridge_topic_exchange";
-  const TEST_PG_CHANNEL: &str = "pgchannel3";
+//fn publishing_to_topic_exchange_works() {
+  //const TEST_QUEUE_1: &str = "pg_amqp_bridge_queue_3";
+  //const TEST_QUEUE_2: &str = "pg_amqp_bridge_queue_4";
+  //const TEST_EXCHANGE: &str = "pg_amqp_bridge_topic_exchange";
+  //const TEST_PG_CHANNEL: &str = "pgchannel3";
+
+  //let mut core = Core::new().unwrap();
+  //let handle = core.handle();
+  //let addr = TEST_AMQP_HOST_PORT.parse().unwrap();
+
+  //let _ = core.run(
+    //TcpStream::connect(&addr, &handle)
+    //.and_then(|stream| Client::connect(stream, &ConnectionOptions::default()) )
+    //.and_then(|client| client.create_channel())
+    //.and_then(|channel|{
+      //let ch1 = channel.clone();
+      //let ch2 = channel.clone();
+      //let ch3 = channel.clone();
+      //channel.queue_declare(TEST_QUEUE_1, &QueueDeclareOptions::default(), FieldTable::new())
+      //.and_then(move |_|
+        //channel.queue_declare(TEST_QUEUE_2, &QueueDeclareOptions::default(), FieldTable::new())
+        //.and_then(move |_|
+          //channel.exchange_declare(TEST_EXCHANGE, "topic",
+                                                //&ExchangeDeclareOptions{
+                                                  //passive: false, durable: false,
+                                                  //auto_delete: true, internal: false,
+                                                  //nowait: false
+                                                //}, FieldTable::new())
+          //.and_then(move |_|
+            //channel.queue_bind(TEST_QUEUE_1, TEST_EXCHANGE, "*.critical", &QueueBindOptions::default(), FieldTable::new())
+            //.and_then(move |_| channel.queue_bind(TEST_QUEUE_2, TEST_EXCHANGE, "*.info", &QueueBindOptions::default(), FieldTable::new()))
+          //)
+        //)
+      //)
+      //.and_then(move |_|
+        //ch1.basic_consume(TEST_QUEUE_1, "my_consumer_3", &BasicConsumeOptions::default())
+        //.and_then(move |stream| {
+          //let pg_conn = Connection::connect(TEST_PG_URI, TlsMode::None).unwrap();
+          //let _ = pg_conn.execute(format!("NOTIFY {}, 'kern.critical|Kernel critical message'", TEST_PG_CHANNEL).as_str(), &[]);
+          //stream.into_future().map_err(|(err, _)| err)
+        //})
+        //.and_then(move |(message, _)| {
+          //let msg = message.unwrap();
+          //assert_eq!(msg.data, b"Kernel critical message");
+          //ch1.basic_ack(msg.delivery_tag)
+        //})
+      //)
+      //.and_then(move |_|
+        //ch2.basic_consume(TEST_QUEUE_2, "my_consumer_4", &BasicConsumeOptions::default())
+        //.and_then(move |stream| {
+          //let pg_conn = Connection::connect(TEST_PG_URI, TlsMode::None).unwrap();
+          //let _ = pg_conn.execute(format!("NOTIFY {}, 'user.info|User info message'", TEST_PG_CHANNEL).as_str(), &[]);
+          //stream.into_future().map_err(|(err, _)| err)
+        //})
+        //.and_then(move |(message, _)| {
+          //let msg = message.unwrap();
+          //assert_eq!(msg.data, b"Anon info message");
+          //ch2.basic_ack(msg.delivery_tag)
+        //})
+      //)
+      //.then(move |_|
+        //ch3.queue_delete(TEST_QUEUE_1, &QueueDeleteOptions::default())
+        //.then(move |_| ch3.queue_delete(TEST_QUEUE_2, &QueueDeleteOptions::default()))
+       //)
+    //})
+  //);
+//}
+
+/*
+ * This is to pass validation of the bridge, the queues still need to be
+ * redeclared in the tests due to the inability of using an undeclared queue in a channel
+ * See this issue: https://github.com/sozu-proxy/lapin/issues/32
+*/
+fn setup(){
+  const TEST_1_QUEUE: &str = "pg_amqp_bridge_queue_1";
+  const TEST_2_QUEUE: &str = "pg_amqp_bridge_queue_2";
+  const TEST_2_EXCHANGE: &str = "pg_amqp_bridge_direct_exchange";
+
+  let mut core = Core::new().unwrap();
+  let handle = core.handle();
+  let addr = TEST_AMQP_HOST_PORT.parse().unwrap();
+
+  let _ = core.run(
+    TcpStream::connect(&addr, &handle)
+    .and_then(|stream| Client::connect(stream, &ConnectionOptions::default()) )
+    .and_then(|client| client.create_channel())
+    .and_then(|channel|
+      channel.queue_declare(TEST_1_QUEUE, &QueueDeclareOptions::default(), FieldTable::new())
+      .and_then(move |_| 
+        channel.queue_declare(TEST_2_QUEUE, &QueueDeclareOptions::default(), FieldTable::new())
+        .and_then(move |_| 
+          channel.exchange_declare(TEST_2_EXCHANGE, "direct", 
+                                   &ExchangeDeclareOptions{
+                                     passive: false,
+                                     durable: false,
+                                     auto_delete: true,
+                                     internal: false,
+                                     nowait: false
+                                   }, FieldTable::new())
+        )
+      )
+    )
+  );
+}
+
+fn teardown(){
+  const TEST_1_QUEUE: &str = "pg_amqp_bridge_queue_1";
+  const TEST_2_QUEUE: &str = "pg_amqp_bridge_queue_2";
 
   let mut core = Core::new().unwrap();
   let handle = core.handle();
@@ -114,55 +223,8 @@ fn publishing_to_topic_exchange_works() {
     .and_then(|stream| Client::connect(stream, &ConnectionOptions::default()) )
     .and_then(|client| client.create_channel())
     .and_then(|channel|{
-      let ch1 = channel.clone();
-      let ch2 = channel.clone();
-      let ch3 = channel.clone();
-      channel.queue_declare(TEST_QUEUE_1, &QueueDeclareOptions::default(), FieldTable::new())
-      .and_then(move |_|
-        channel.queue_declare(TEST_QUEUE_2, &QueueDeclareOptions::default(), FieldTable::new())
-        .and_then(move |_|
-          channel.exchange_declare(TEST_EXCHANGE, "topic",
-                                                &ExchangeDeclareOptions{
-                                                  passive: false, durable: false,
-                                                  auto_delete: true, internal: false,
-                                                  nowait: false
-                                                }, FieldTable::new())
-          .and_then(move |_|
-            channel.queue_bind(TEST_QUEUE_1, TEST_EXCHANGE, "*.critical", &QueueBindOptions::default(), FieldTable::new())
-            .and_then(move |_| channel.queue_bind(TEST_QUEUE_2, TEST_EXCHANGE, "*.info", &QueueBindOptions::default(), FieldTable::new()))
-          )
-        )
-      )
-      .and_then(move |_|
-        ch1.basic_consume(TEST_QUEUE_1, "my_consumer_3", &BasicConsumeOptions::default())
-        .and_then(move |stream| {
-          let pg_conn = Connection::connect(TEST_PG_URI, TlsMode::None).unwrap();
-          let _ = pg_conn.execute(format!("NOTIFY {}, 'kern.critical|Kernel critical message'", TEST_PG_CHANNEL).as_str(), &[]);
-          stream.into_future().map_err(|(err, _)| err)
-        })
-        .and_then(move |(message, _)| {
-          let msg = message.unwrap();
-          assert_eq!(msg.data, b"Kernel critical message");
-          ch1.basic_ack(msg.delivery_tag)
-        })
-      )
-      .and_then(move |_|
-        ch2.basic_consume(TEST_QUEUE_2, "my_consumer_4", &BasicConsumeOptions::default())
-        .and_then(move |stream| {
-          let pg_conn = Connection::connect(TEST_PG_URI, TlsMode::None).unwrap();
-          let _ = pg_conn.execute(format!("NOTIFY {}, 'user.info|User info message'", TEST_PG_CHANNEL).as_str(), &[]);
-          stream.into_future().map_err(|(err, _)| err)
-        })
-        .and_then(move |(message, _)| {
-          let msg = message.unwrap();
-          assert_eq!(msg.data, b"Anon info message");
-          ch2.basic_ack(msg.delivery_tag)
-        })
-      )
-      .then(move |_|
-        ch3.queue_delete(TEST_QUEUE_1, &QueueDeleteOptions::default())
-        .then(move |_| ch3.queue_delete(TEST_QUEUE_2, &QueueDeleteOptions::default()))
-       )
+      channel.queue_delete(TEST_1_QUEUE, &QueueDeleteOptions::default())
+      .and_then(move |_| channel.queue_delete(TEST_2_QUEUE, &QueueDeleteOptions::default()))
     })
   );
 }
@@ -181,6 +243,7 @@ fn add_test<F: FnOnce() + Send + 'static>(tests: &mut Vec<TestDescAndFn>, name: 
 fn main() {
   let args: Vec<_> = env::args().collect();
   let mut tests = Vec::new();
+  setup();
   add_test(&mut tests, "publishing_to_queue_works".to_string(), publishing_to_queue_works);
   add_test(&mut tests, "publishing_to_direct_exchange_works".to_string(), publishing_to_direct_exchange_works);
   //add_test(&mut tests, "publishing_to_topic_exchange_works".to_string(), publishing_to_topic_exchange_works);
@@ -191,4 +254,6 @@ fn main() {
   );
   thread::sleep(Duration::from_millis(4000));
   test::test_main(&args, tests);
+  teardown();
 }
+
